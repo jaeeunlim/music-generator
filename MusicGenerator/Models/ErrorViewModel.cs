0d52215a91df275c648cd61a022d58caa55af9ad
Esther Lim <esther.lim.719@gmail.com>
2021-10-20 21:07:52 -0400

    * .gitignore: Add a page for music sheet generation interface.
    * MusicGenerator.sln:
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * site.js:
    * sheet.js:
    * favicon.ico:
    * site.css:
    * home.css:
    * MusicGenerator.csproj:
    * sheet.css:
    * Index.cshtml:
    * Sheet.cshtml:
    * _ViewStart.cshtml:
    * ErrorViewModel.cs:
    * _ViewImports.cshtml:
    * Error.cshtml:
    * Privacy.cshtml:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * HomeController.cs:
    * LICENSE:
    * LICENSE.txt:
    * blank_sheet.png:
    * launchSettings.json:
    * jquery.js:
    * home-background.jpeg:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.js.map:
    * bootstrap.min.js:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * jquery.validate.js:
    * LICENSE.txt:
    * bootstrap-grid.min.css:
    * bootstrap.bundle.min.js:
    * bootstrap.bundle.js.map:
    * bootstrap-grid.css.map:
    * bootstrap-reboot.min.css:
    * bootstrap-reboot.css.map:
    * additional-methods.js:
    * bootstrap.bundle.min.js.map:
    * jquery.validate.min.js:
    * bootstrap-grid.min.css.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ MusicGenerator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MusicGenerator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MusicGenerator/wwwroot/js/sheet.js
@@ -0,0 +1,28 @@
+﻿const MIN_SHEETS = 1;
+var num_sheets = 1;
+
+// Add more music sheet when Add button is clicked.
+document.getElementById("btn-add").onclick = function () {
+    var container = document.getElementById("sheet-group");
+    var section = document.getElementById(`sheet1`);
+    var clone = section.cloneNode(true);
+    num_sheets++;
+    clone.id = `sheet{$num_sheets}`;
+    container.appendChild(clone);
+
+    if (num_sheets > MIN_SHEETS) {
+        document.getElementById("btn-remove").hidden = false;
+    }
+}
+
+// Delete music sheet when Remove button is clicked.
+document.getElementById("btn-remove").onclick = function () {
+    if (num_sheets > MIN_SHEETS) {
+        document.getElementById(`sheet{$num_sheets}`).remove();
+        num_sheets--;
+
+        if (num_sheets == MIN_SHEETS) {
+            document.getElementById("btn-remove").hidden = true;
+        }
+    }
+}
