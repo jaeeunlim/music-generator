411bf084736dbfd75d653a306357e4f52e6c7d92
martinus-hbg <tinus.mmm02@gmail.com>
2021-11-12 20:15:38 -0500

    Added Business Access and DataAccess Layer

---
+++ DataAccess/MusicGenerator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+
+namespace DataAccess
+{
+    public class MusicGenerator
+    {
+
+        public static DataSet GetStavesByMusicId(int musicId, IConfiguration config)
+        {
+            var cs = config.GetConnectionString("MySQLConnectionString");
+            return null;
+        }
+    }
+}
